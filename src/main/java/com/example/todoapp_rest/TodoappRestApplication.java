fa081130c6d20936d7449f114858029e55642471
Miguel Monzó <mikemonzo@gmail.com>
2025-04-17 18:30:12 +0200

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/com/example/todoapp_rest/TodoappRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapp_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoapp_rest/TodoappRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoapp_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
